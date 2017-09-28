cc57b186c385f690654a60676bd9800fcc04257a
An Le <lean257@gmail.com>
2017-09-28 13:14:27 -0400

    fix: linter and component name

--- app/components/Welcome.jsx
+++ app/components/Welcome.jsx
@@ -5,13 +5,13 @@ import Modal from 'react-modal'
 import fire from '../../fire'
 
 const customStyles = {
-  content : {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
   }
 }
 
