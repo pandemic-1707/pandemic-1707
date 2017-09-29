95ff77af0d9e8978cec41fb3a4b37da688f68e58
Mary Yen <mymaryyn@gmail.com>
2017-09-29 10:49:41 -0400

    refactor: removing reducers

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,22 +1,15 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-import { initialShufflePlayerDeck } from '../store/playerDeck.js'
-
 export class Home extends Component {
   constructor(props) {
     super(props)
   }
 
   componentWillMount() {
-    // TODO: change num players to be modifiable
-    const NUM_PLAYERS = 4
-    this.props.initializeGame(NUM_PLAYERS)
   }
 
   render() {
-    const { playerDeck, players } = this.props
-    console.log("render deck", playerDeck)
     return (
       <div class="title">
         <h1 id="gametitle">Pandemic</h1>
@@ -25,18 +18,4 @@ export class Home extends Component {
   }
 }
 
-const mapStateToProps = function(state) {
-  return {
-    playerDeck: state.playerDeck,
-    players: state.players
-  }
-}
-const mapDispatchToProps = function(dispatch) {
-  return {
-    initializeGame: function(numPlayers) {
-      dispatch(initialShufflePlayerDeck())
-    }
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home)
+export default (Home)
