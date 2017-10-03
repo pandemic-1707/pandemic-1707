83f3c94055048e5518e66b8dddf4ada39daa8846
An Le <lean257@gmail.com>
2017-10-03 00:19:57 -0400

    fix: link to avatar

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -22,13 +22,12 @@ export default class Sidebar extends Component {
     const players = Object.values(this.state.players).map((player, idx) => {
       const playerOrder = {'player1': '#FF339F', 'player2': '#30CA8D', 'player3': '#FFA913', 'player4': '#A213FF'}
       var color = playerOrder[`player${idx+1}`]
-      console.log('player hand', this.state.players[`player${idx+1}`])
       return (
         <div key={idx}>
           <div>
             <div className="player-box">
               <div className="player-name" style={{backgroundColor: color}}>
-                <img height="32" width="32" src={`/${player.role}.png`} />
+                <img height="32" width="32" src={`/images/${player.role}.png`} />
                 <div>
                   {player.name}
                 <br />
