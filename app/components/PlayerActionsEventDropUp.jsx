945cf7124b199637975b9d9030ee09e315516219
Mary Yen <mymaryyn@gmail.com>
2017-10-03 17:23:25 -0400

    feat: listed nearby locations

--- app/components/PlayerActionsEventDropUp.jsx
+++ app/components/PlayerActionsEventDropUp.jsx
@@ -2,26 +2,18 @@ import React, { Component } from 'react'
 import { Dropdown } from 'semantic-ui-react'
 import fire from '../../fire'
 
-// TODO: remove - from city names display
-
-export default class PlayerActionMoveDropUp extends Component {
+export default class PlayerActionsEventDropUp extends Component {
   constructor(props) {
     super(props)
     this.state = {
       players: {},
       selectedCityCondition: false,
-      selectedCity: '',
-      cities: []
+      selectedCity: ''
     }
   }
 
   componentDidMount() {
-    // set local state to firebase state
-    fire.database().ref(`/rooms/${this.props.roomName}/cities`).on('value', snapshot => {
-      this.setState({
-        cities: snapshot.val()
-      })
-    })
+
   }
 
   handleChange = (e) => {
@@ -42,37 +34,20 @@ export default class PlayerActionMoveDropUp extends Component {
     if (this.state.selectedCityCondition) return <button onClick={this.handleConfirm} >Confirm</button>
   }
 
-  getNearbyCities = (cityName) => {
-    if (this.state.cities[cityName]) {
-      return this.state.cities[cityName].connections
-    }
-  }
-
   render() {
     let confirmButton = this.showConfirm()
-    const nearbyCities = this.props.activePlayer && this.props.activePlayer.position && this.getNearbyCities(this.props.activePlayer.position.city)
     return (
       <div className="ui form" onSubmit={this.handleConfirm}>
         <div className="field">
           <select className="ui upward dropdown" onChange={this.handleChange} value={this.state.value}>
             <option value="" disabled selected hidden>Move</option>
-            <optgroup label="Nearby (drive/ferry)">
-              {
-                nearbyCities && nearbyCities.map((city) => {
-                  return <option key={city} value={city}>{city}</option>
-                })
+            {
+              this.props.activePlayer && this.props.activePlayer.hand && this.props.activePlayer.hand.map((card) => {
+                if (card.city) {
+                  return <option key={card.city} value={card.city}>{card.city}</option>
+                }
               })
-              }
-            </optgroup>
-            <optgroup label="Player Hand (direct flight)">
-              {
-                this.props.activePlayer && this.props.activePlayer.hand && this.props.activePlayer.hand.map((card) => {
-                  if (card.city) {
-                    return <option key={card.city} value={card.city}>{card.city}</option>
-                  }
-                })
-              }
-            </optgroup>
+            }
           </select>
           {confirmButton}
         </div>
