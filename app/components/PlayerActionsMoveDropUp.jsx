3684700123926181e7078606ac6286f04c89b769
Mary Yen <mymaryyn@gmail.com>
2017-10-03 18:31:27 -0400

    feat: players move to their location on map

--- app/components/PlayerActionsMoveDropUp.jsx
+++ app/components/PlayerActionsMoveDropUp.jsx
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 import { Dropdown } from 'semantic-ui-react'
 import fire from '../../fire'
 
-// TODO: remove - from city names display
+// TODO: remove - hyphens from city names display
+// TODO: charter flight: if player has city they're located in hand, allow them to pick any city on the PLANET
+// TODO: shuttle: travel to research stations
 
 export default class PlayerActionMoveDropUp extends Component {
   constructor(props) {
@@ -40,15 +42,17 @@ export default class PlayerActionMoveDropUp extends Component {
     this.setState({ selectedCityCondition: true })
   }
 
+  // todo: will there be a bug if the players somehow hit confirm before getting db callbacks?
   handleConfirm = (e) => {
     e.preventDefault()
     const moveToCity = this.state.selectedCity
-    // remove city card if used
+    // update hand without any used city cards
     const newHand = this.props.activePlayer.hand.filter(function(card) {
       return moveToCity !== card.city
     })
+    // update position and num actions left
     fire.database().ref(`/rooms/${this.props.roomName}/players/${this.props.activePlayer.playerName}`).update({
-      position: {city: moveToCity, location: [33.748995, -84.387982]}, // TODO: update location coordinates
+      position: {city: moveToCity, location: this.state.cities[moveToCity].location}, // TODO: update location coordinates
       numActions: this.props.numActions - 1,
       hand: newHand
     })
