127e184ce8e152373917a13adc817a8aa31b3acb
Mary Yen <mymaryyn@gmail.com>
2017-10-04 14:51:56 -0400

    refactor: trying to get a little dry wip

--- app/components/PlayerActionsMoveDropUp.jsx
+++ app/components/PlayerActionsMoveDropUp.jsx
@@ -13,8 +13,10 @@ export default class PlayerActionMoveDropUp extends Component {
       players: {},
       selectedCityCondition: false,
       selectedCity: '',
+      selectedType: '',
       cities: []
     }
+    this.activePlayerCity = this.props.activePlayer && this.props.activePlayer.position
   }
 
   componentDidMount() {
@@ -38,7 +40,8 @@ export default class PlayerActionMoveDropUp extends Component {
 
   // changes to the dropdown selection
   handleChange = (e) => {
-    this.setState({ selectedCity: e.target.value })
+    this.setState({ selectedCity: e.target.value.name })
+    this.setState({ selectedType: e.target.value.type })
     this.setState({ selectedCityCondition: true })
   }
 
@@ -47,9 +50,14 @@ export default class PlayerActionMoveDropUp extends Component {
     e.preventDefault()
     const moveToCity = this.state.selectedCity
     // update hand without any used city cards
-    const newHand = this.props.activePlayer.hand.filter(function (card) {
-      return moveToCity !== card.city
-    })
+    let newHand = []
+    if (this.state.selectedType === 'hand' || this.state.selectedType === 'charter') {
+      newHand = this.props.activePlayer.hand.filter(function(card) {
+        return moveToCity !== card.city
+      })
+    } else {
+      newHand = this.props.activePlayer.hand
+    }
     // update position and num actions left
     fire.database().ref(`/rooms/${this.props.roomName}/players/${this.props.activePlayer.playerName}`).update({
       position: { city: moveToCity, location: this.state.cities[moveToCity].location }, // TODO: update location coordinates
@@ -59,19 +67,29 @@ export default class PlayerActionMoveDropUp extends Component {
     this.setState({ selectedCityCondition: false })
   }
 
+  // TODO: error where if you travel to nearby city, if it's in your hand, it will remove it!!
+  // need to check which submenu player chose
   render() {
     let confirmButton = this.props.numActions && this.showConfirm()
-    const activePlayerCity = this.props.activePlayer && this.props.activePlayer.position
     const nearbyCities = this.props.activePlayer && this.props.activePlayer.position && this.getNearbyCities(this.props.activePlayer.position.city)
-    if (this.props.activePlayer && this.props.activePlayer.position) {
-      const activePlayerCity = this.props.activePlayer && this.props.activePlayer.position.city
-      const canCharter = this.props.activePlayer && this.props.activePlayer.hand && this.props.activePlayer.hand.some((card) => {
-        return activePlayerCity === card.city
-      })
-      console.log("CHARTER", canCharter)
-    }
-    
+    // check if charter available
+    let charter = null
     const allCities = this.state.cities && Object.keys(this.state.cities)
+    if (this.props.activePlayer && this.props.activePlayer.position) {
+      const charterCity = this.props.activePlayer && this.props.activePlayer.hand && this.props.activePlayer.hand.find((card) => {
+        return this.activePlayerCity === card.city
+      })
+      if (charterCity) {
+        charter =
+          <optgroup label={`Use ${charterCity.city} to charter flight to ANYWHERE`}>
+            {allCities && allCities.length && allCities.map(function(city) {
+              return <option key={city} value={{ name: city, type: 'charter' }}>{city}</option>
+            })
+            }
+          </optgroup>
+      }
+    }
+
     return (
       <div className="ui form" onSubmit={this.handleConfirm}>
         <div className="field">
@@ -80,7 +98,7 @@ export default class PlayerActionMoveDropUp extends Component {
             <optgroup label="Nearby (drive/ferry)">
               {
                 nearbyCities && nearbyCities.map((city) => {
-                  return <option key={city} value={city}>{city}</option>
+                  return <option key={city} value={{ name: city, type: 'nearby' }}>{city}</option>
                 })
               })
               }
@@ -89,21 +107,15 @@ export default class PlayerActionMoveDropUp extends Component {
               {
                 this.props.activePlayer && this.props.activePlayer.hand && this.props.activePlayer.hand.map((card) => {
                   if (card.city) {
-                    return <option key={card.city} value={card.city}>{card.city}</option>
+                    return <option key={card.city} value={{ name: card.city, type: 'hand' }}>{card.city}</option>
                   }
                 })
               }
             </optgroup>
             {/* display only if you have a card matching your current city */}
-            <optgroup label="All Locations">
-
-              {
-                allCities.length && allCities.map(function (city) {
-                  return <option key={city} value={city}>{city}</option>
-                })
-              }
-
-            </optgroup>
+            {
+              charter
+            }
           </select>
           {confirmButton && <button onClick={this.handleConfirm} >Confirm</button>}
         </div>
