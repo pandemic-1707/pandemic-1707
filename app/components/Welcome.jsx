0009e84128aeff5bc9e179029e4e75b78b5b55d8
ebeastlake <ebeastlake@gmail.com>
2017-10-05 21:58:50 -0400

    wip: dummy rules for beta-testing

--- app/components/Welcome.jsx
+++ app/components/Welcome.jsx
@@ -37,6 +37,7 @@ export default class Welcome extends Component {
     super(props)
     this.state = {
       modalIsOpen: false,
+      rulesOpen: false,
       roomName: 'one',
       numPlayers: 2,
       players: {
@@ -65,9 +66,15 @@ export default class Welcome extends Component {
     this.saveRoomData = this.saveRoomData.bind(this)
     this.savePlayerName = this.savePlayerName.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.openRules = this.openRules.bind(this)
+    this.closeRules = this.closeRules.bind(this)
   }
   openModal() {
     this.setState({modalIsOpen: true})
+  }
+
+  openRules() {
+    this.setState({rulesOpen: true})
   }
 
   saveRoomData(field, e) {
@@ -132,6 +139,10 @@ export default class Welcome extends Component {
 
   closeModal() {
     this.setState({modalIsOpen: false})
+  }
+
+  closeRules() {
+    this.setState({rulesOpen: false})
   }
 
   render() {
@@ -224,7 +235,37 @@ export default class Welcome extends Component {
           </form>
           </Modal>
           <br />
-          <button className="btn btn-outline-success">Rules</button>
+          <button className="btn btn-outline-success" onClick={this.openRules}>Rules</button>
+          <Modal
+          isOpen={this.state.rulesOpen}
+          onRequestClose={this.closeRules}
+          style={customStyles}
+          contentLabel="Example Modal"
+          >
+            <button type="button"
+              className="close"
+              aria-label="Close"
+              onClick={this.closeRules}
+              >
+              <span aria-hidden="true">&times;</span>
+            </button>
+            <div>
+              <h3>In this game, you can't lose (at least not yet).</h3>
+              <h4>The question is: how quickly can you eradicate the disease?</h4>
+              <h3>How to Play:</h3>
+              <p> Work together as a team to cure a deadly pandemic as quickly as possible as it spreads across the world map. </p>
+              <p> The board will show you the initial infection levels in each city (3 being the highest and 1 being the lowest). </p>
+              <p> On your turn, you may take up to four actions to fight for your survival.
+              Options include: 1) moving to an adjacent city, 2) discarding a card to move to the named city,
+              3) discarding a card matching your current city to move to any city or 4) removing one disease cube from your current city. </p>
+              <p> After your four actions, you will draw a card from the player deck. Most of these are city cards,
+              but some are Special Event cards which cannot be played at this time. </p>
+              <p> If you draw an Epidemic card, we will draw the bottom card of the infection deck and place three cubes in that city,
+              causing an outbreak if necessary. (If a fourth disease cube would ever be added to a city, that city suffers an outbreak,
+              spreading cubes to all adjacent cities.) </p>
+              <p> When you remove all the infection from the board, you have won! </p>
+            </div>
+          </Modal>
         </div>
       </div>
     )
