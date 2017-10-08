6461d93421cd168e6cdaf85f26cc8277e79a75fe
Mary Yen <mymaryyn@gmail.com>
2017-10-08 18:42:48 -0400

    wip feat: cure button appears depending on if curable

--- app/components/PlayerActions.jsx
+++ app/components/PlayerActions.jsx
@@ -17,7 +17,8 @@ export default class PlayerActions extends Component {
     this.state = {
       players: {},
       cities: {},
-      currPlayer: ''
+      currPlayer: '',
+      cureCards: []
     }
 
     this.handleClick = this.handleClick.bind(this)
@@ -98,7 +99,7 @@ export default class PlayerActions extends Component {
           })
             .then(() => {
               // discard used city card by creating newHand without it
-              const newHand = activePlayer.hand.filter(function(card) {
+              const newHand = activePlayer.hand.filter(function (card) {
                 return card.city !== buildInCity.city
               })
               fire.database().ref(`/rooms/${this.props.roomName}/players/${activePlayer.playerKey}`).update({
@@ -121,40 +122,69 @@ export default class PlayerActions extends Component {
   // canCureDisease > displayCardsForCure > handleCureCardChange, handleCureCardConfirm > cureDisease > change treat func so that it will auto cure all disease
 
   // check if it's possible to cure disease at this city
-    // TODO: refactor to only have to check if research city once 
+  // TODO: refactor to only have to check if research city once 
   // returns { curableColors: curableColors, sameColors: sameColors }
   // sameColors has key=color, value = array of cards of same color
   // curableColors is array of curable colors
-    // if we have no curableColors, deactivate cure button
+  // if we have no curableColors, deactivate cure button
   canCureDisease = (activePlayer, allCities) => {
     // are we in a research city?
-    console.log("ACTIVE PLAYER", activePlayer)
-    console.log("ALL CITIES", allCities)
-    const activePlayerCity = activePlayer.position.city
+    const activePlayerCity = activePlayer && activePlayer.position && activePlayer.position.city
     const researchCity = Object.keys(allCities).find(function (city) {
       if (city === activePlayerCity && allCities[city].research === true) {
         return city
       }
     })
-    // do we have 5 city cards of the same color
-    const sameColors = {}
-    activePlayer.hand.forEach(function (card) {
-      if (card.props) {
-        if (sameColors[card.props.color]) sameColors[card.props.color].push(card)
-        else sameColors[card.props.color] = [card]
-      }
+    if (researchCity) {
+      // do we have 5 city cards of the same color
+      const sameColors = {}
+      activePlayer.hand.forEach(function (card) {
+        if (card.props) {
+          if (sameColors[card.props.color]) sameColors[card.props.color].push(card)
+          else sameColors[card.props.color] = [card]
+        }
+      })
+      const curableColors = []
+      Object.keys(sameColors).map(function (color) {
+        if (sameColors[color] >= NUM_CARDS_FOR_CURE) curableColors.push(color)
+      })
+      if (curableColors.length) return { curableColors: curableColors, sameColors: sameColors }
+      else return false
+    }
+    return false
+  }
+
+  // cureCards is array of cards to be discarded for cure
+  cureDisease = () => {
+    const activePlayer = this.getActivePlayer(this.state.players)
+    const activePlayerCity = activePlayer && activePlayer.position && activePlayer.position.city
+    // look for cure cards to discard and create newHand without the cure cards
+    const cureCards = this.state.cureCards
+    const newHand = activePlayer.hand.filter(function (card) {
+      // newHand can't have any cards we want to discard
+      return cureCards.every(function (cureCards) {
+        return card.city !== cureCards.city
+      })
     })
-    const curableColors = Object.keys(sameColors).map(function (color) {
-      if (sameColors[color] >= NUM_CARDS_FOR_CURE) return color
+    fire.database().ref(`/rooms/${this.props.roomName}/players/${activePlayer.playerKey}`).update({
+      numActions: activePlayer.numActions - 1,
+      hand: newHand
     })
-    console.log("CAN CURE", { curableColors: curableColors, sameColors: sameColors })
-    return { curableColors: curableColors, sameColors: sameColors }
+    // add to curedDiseases
+    fire.database().ref(`/rooms/${this.props.roomName}/state`).update({
+      curedDiseases: []
+    })
+    fire.database().ref(`/rooms/${this.props.roomName}/state`).update({
+      curedDiseases: []
+    })
+
+    // TODO: ... have treat disease clear all 3 infection rate for the color 
   }
 
   render() {
     const activePlayer = this.state.players && this.getActivePlayer(this.state.players)
     const allCities = this.state.cities
-    if (activePlayer.position && allCities) this.canCureDisease(activePlayer, allCities)
+    const canCure = this.canCureDisease(activePlayer, allCities)
     return (
       <div>
         <div className="container-fluid player-actions-panel">
@@ -166,7 +196,9 @@ export default class PlayerActions extends Component {
               <button onClick={this.treatDisease}>Treat</button>
             </div>
             <div className="col-sm-2 player-action text-center">
-              <span>Cure</span>
+              {
+                canCure && <button onClick={this.cureDisease}>Cure</button>
+              }
             </div>
             <div className="col-sm-2 player-action text-center">
               <button onClick={this.buildResearch}>Build</button>
